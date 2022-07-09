59398ab13dfa20f950280639ad5e564612125313
Shreysh Mane <shreysh.mane12@gmail.com>
2022-07-09 17:57:09 +0530

    update: requestMapping

--- SpringBoot/springdemo/src/main/java/com/revature/springdemo/controller/UserController.java
+++ SpringBoot/springdemo/src/main/java/com/revature/springdemo/controller/UserController.java
@@ -1,17 +1,26 @@
 package com.revature.springdemo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.springdemo.model.User;
 
 //@Controller
 @RestController
 @CrossOrigin("*")
+@RequestMapping("/api/user")
 public class UserController {
 	
 //	@RequestMapping(value="/getuser",method = RequestMethod.GET )
@@ -19,24 +28,80 @@ public class UserController {
 //	public void getUser() {
 //		System.out.println("fetching the user");
 //	}
+//		
+
+//	@GetMapping(value="/getuser")
+//	public void getUser() {
+//		System.out.println("fetching the user");
+//	}
 //	
+//	@GetMapping(value="/getusers")
+//	@ResponseBody
+//	public String getUsers() {
+//		return "gettin users";
+//	}
+//	
+
+//	@GetMapping(value="/getusers")
+//	public String getUsers() {
+//		return "getting users";
+//	}
+
 	
+//	@GetMapping(value="/users")
+//	public String getAllUser() {
+//		System.out.print("got the req dwdw");
+//		return "Getting all the users in db";
+//		
+//	
+//	}
+	
+	@GetMapping(value="/users")
+	public List<User> getAllUser() {
+		System.out.print("got the req dwdw");
+		User u=new User();
+		u.setName("shre");
+		
+		User u1=new User();
+		u1.setName("shre");
+		
+		List<User> ul=new ArrayList<User>();
+		ul.add(u1);
+		ul.add(u);
+		
+		return ul;
+		
+		
+	
+	}
+	
+	@GetMapping(value="/userbyid/{id}")
+	public String getUserById(@PathVariable Long id,@PathVariable String name) {
+		return ("Getting user based on id " + id + name);
+	}
 	
 
-	@GetMapping(value="/getuser")
-	public void getUser() {
-		System.out.println("fetching the user");
+	@PostMapping("/finguserwithname")
+	public String prameterdemo(@RequestParam("name") String name,@RequestParam("address") String address) {
+		System.out.println("Hello");
+		System.out.println(name);
+		System.out.println(address);
+		return ("hello" +name + address) ;
 	}
 	
-	@GetMapping(value="/getusername")
-//	@ResponseBody
-	public String getUsername() {
-		System.out.println("getting the user name");
-		return "getting name";
+	
+	@PostMapping("/saveuser")
+	public void saveuser(@RequestBody User user) {
+		System.out.print("hello");
+		System.out.print(user.getName());
+		System.out.print(user.getAddress());
+		
+	//	return "saving user";
 	}
 	
-//  @RequestMapping(value="/getuser",method = RequestMethod.POST )
-//	@PostMapping(value="/getuser")
+	
+	
+	
 	
 	
 }
