2a1385c3d5c4cdb1e7f3c9f24eae49f78603ac5d
Shreysh Mane <shreysh.mane12@gmail.com>
2022-07-15 12:35:05 +0530

    Update:

--- course/src/main/java/com/revature/course/dto/CourseRepoInterface.java
+++
@@ -1,9 +0,0 @@
-package com.revature.course.dto;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.revature.course.model.Course;
-
-public interface CourseRepoInterface extends JpaRepository<Course, Long>{
-
-}
