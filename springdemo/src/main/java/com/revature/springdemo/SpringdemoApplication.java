8707ea0a40ea352f2eb03d3815b08d73ff79d4ce
Shreysh Mane <shreysh.mane12@gmail.com>
2022-07-18 16:05:10 +0530

    update : docker

--- springdemo/src/main/java/com/revature/springdemo/SpringdemoApplication.java
+++ springdemo/src/main/java/com/revature/springdemo/SpringdemoApplication.java
@@ -2,18 +2,10 @@ package com.revature.springdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringdemoApplication extends SpringBootServletInitializer {
+public class SpringdemoApplication {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringdemoApplication.class);
-	}
-	
-	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringdemoApplication.class, args);
 	}
